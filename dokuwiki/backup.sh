02d8f49cf8354be0b42e72ff62eea13fbc0d461d
William Hanson <william_hanson@hotmail.com>
2022-11-30 09:32:03 +0000

    Update changes

---
+++ dokuwiki/backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Put this in /etc/cron.hourly/backup
+TARGET="gs://dokuwiki-storage-bucket"
+
+tar_file=/tmp/dokuwiki-backup-$(date +%s).tar.gz
+tar -czf $tar_file /var/www/html 2>/dev/null
+/snap/bin/gsutil cp $tar_file $TARGET
+rm -f $tar_file
+
--- dokuwiki/main.tf
+++ dokuwiki/main.tf
@@ -1,7 +1,7 @@
 /*
 William Hanson
 Cabrillo College, CIS-91, Fall 2022
-Assignment: Project 2
+Assignment: Project2-dokuwiki
 11/22/2022
 */
 
@@ -46,13 +46,18 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "cis91"
+  name         = "dokuwiki"
   machine_type = var.instance_type
 
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
+  }
+  
+  attached_disk {
+    source = google_compute_disk.dokuwiki-persistent.self_link
+    device_name = "dokuwiki-persistent"
   }
 
   network_interface {
@@ -72,6 +77,56 @@ resource "google_compute_firewall" "default-firewall" {
   source_ranges = ["0.0.0.0/0"]
 }
 
+resource "google_compute_disk" "dokuwiki-persistent" {
+  name  = "dokuwiki-persistent"
+  type  = "pd-ssd"
+  labels = {
+    environment = "dokuwiki"
+  }
+  size = "100"
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/iam.serviceAccountAdmin"
+    members = [
+      "serviceAccount:${google_service_account.dokuwiki-service-account.email}",
+    ]
+  }
+}
+
+data "google_iam_policy" "bucket_policy" {
+  binding {
+    role = "roles/storage.admin"
+    members = [
+      "serviceAccount:${google_service_account.dokuwiki-service-account.email}",
+      "user:william_hanson@hotmail.com",
+    ]
+  }
+}
+
+resource "google_service_account" "dokuwiki-service-account" {
+  account_id   = "dokuwiki-service-account"
+  display_name = "dokuwiki-service-account"
+  description = "Service account for Dokuwiki"
+}
+
+resource "google_service_account_iam_policy" "admin-acc-iam" {
+  service_account_id = google_service_account.dokuwiki-service-account.name
+  policy_data        = data.google_iam_policy.admin.policy_data
+}
+
+resource "google_storage_bucket_iam_policy" "policy" {
+  bucket      = google_storage_bucket.dokuwiki-storage-bucket.name
+  policy_data = data.google_iam_policy.bucket_policy.policy_data
+}
+
+// create storage bucket
+resource "google_storage_bucket" "dokuwiki-storage-bucket" {
+  name = "dokuwiki-storage-bucket"
+  storage_class = "STANDARD"
+  location = "us-west1"
+}
 output "external-ip" {
   value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
